5185026faea54e3b62c3b91889c63ff035a435f2
Siidney Watson <siidney.watson.work@gmail.com>
2016-09-14 17:12:50 -0600

    Various style fixes

--- public/js/themetwo.js
+++ public/js/themetwo.js
@@ -10,7 +10,7 @@
 /*
  * Add bookmarks menus to relevant panel
  */
-document.getElementById("panel-top-left").appendChild(document.getElementById("bl-0"));
-document.getElementById("panel-top-right").appendChild(document.getElementById("bl-1"));
-document.getElementById("panel-bottom-left").appendChild(document.getElementById("bl-2"));
-document.getElementById("panel-bottom-right").appendChild(document.getElementById("bl-3"));
+document.getElementById("panel-top-left").appendChild(document.getElementById("bl-1"));
+document.getElementById("panel-top-right").appendChild(document.getElementById("bl-0"));
+document.getElementById("panel-bottom-left").appendChild(document.getElementById("bl-3"));
+document.getElementById("panel-bottom-right").appendChild(document.getElementById("bl-2"));
